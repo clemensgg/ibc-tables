ce746376f14d99619996ee0bf6b01e84d892f6b9
clemensgg <csc@soundportal.at>
2023-06-07 14:49:02 +0200

    Add error handling, update workflow

--- get_ibc_info.sh
+++ get_ibc_info.sh
@@ -5,7 +5,11 @@ PAGE_KEY=""
 CHANNELS=""
 
 while true; do
-  RESPONSE=$(curl -s "${NODE}/ibc/core/channel/v1/channels?pagination.key=${PAGE_KEY}")
+  RESPONSE=$(curl -s --max-time 5 "${NODE}/ibc/core/channel/v1/channels?pagination.key=${PAGE_KEY}")
+  if [[ -z "$RESPONSE" ]]; then
+    echo "Error: Unable to fetch channels from $NODE."
+    exit 1
+  fi
   CHANNELS_ARRAY=$(echo "${RESPONSE}" | jq -c '.channels[]')
   for CHANNEL in $CHANNELS_ARRAY; do
     CHANNELS+="${CHANNEL},"
@@ -22,7 +26,11 @@ PAGE_KEY=""
 CONNECTIONS=""
 
 while true; do
-  RESPONSE=$(curl -s "${NODE}/ibc/core/connection/v1/connections?pagination.key=${PAGE_KEY}")
+  RESPONSE=$(curl -s --max-time 5 "${NODE}/ibc/core/connection/v1/connections?pagination.key=${PAGE_KEY}")
+  if [[ -z "$RESPONSE" ]]; then
+    echo "Error: Unable to fetch connections from $NODE."
+    exit 1
+  fi
   CONNECTIONS_ARRAY=$(echo "${RESPONSE}" | jq -c '.connections[]')
   for CONNECTION in $CONNECTIONS_ARRAY; do
     CONNECTIONS+="${CONNECTION},"
@@ -35,12 +43,20 @@ done
 CONNECTIONS_JSON="[${CONNECTIONS%,}]"
 CONNECTIONS_JSON=$(echo "${CONNECTIONS_JSON}" | jq -r 'sort_by(.id | split("-")[1] | tonumber)')
 
+if [[ -z "$CHANNELS_JSON" || -z "$CONNECTIONS_JSON" ]]; then
+    echo "Error: Unable to fetch data from $NODE."
+    exit 1
+fi
+
 NUM_CHANNELS=$(echo "${CHANNELS_JSON}" | jq '. | length')
-CHAIN_ID=$(curl -s "${NODE}/cosmos/base/tendermint/v1beta1/blocks/latest" | jq -r .block.header.chain_id)
+CHAIN_ID=$(curl -s -m 5 "${NODE}/cosmos/base/tendermint/v1beta1/blocks/latest" | jq -r .block.header.chain_id)
+if [ $? -ne 0 ]; then
+  echo "Error: Unable to fetch latest block from ${NODE}."
+  exit 1
+fi
 echo "CHAIN_ID, CLIENT, CONNECTION, CHANNEL, PORT, COUNTERPARTY_CHAIN, COUNTERPARTY_CLIENT, COUNTERPARTY_CONNECTION, COUNTERPARTY_CHANNEL, COUNTERPARTY_PORT"
 for (( each_channel=0; each_channel < NUM_CHANNELS; each_channel++ ))
 do
-    # Just show the open ones
     STATE=$(echo "${CHANNELS_JSON}" | jq -r ".[${each_channel}].state")
     if [ "$STATE" != "STATE_OPEN" ]; then
             continue
@@ -54,7 +70,11 @@ do
     COUNTERPARTY_CONNECTION=$(echo "${CONNECTION_JSON}" | jq -r .counterparty.connection_id)
     COUNTERPARTY_CLIENT=$(echo "${CONNECTION_JSON}" | jq -r .counterparty.client_id)
     CLIENT=$(echo "${CONNECTION_JSON}" | jq -r .client_id)
-    CLIENT_JSON=$(curl -s "${NODE}/ibc/core/client/v1/client_states/${CLIENT}")
+    CLIENT_JSON=$(curl -s -m 5 "${NODE}/ibc/core/client/v1/client_states/${CLIENT}")
+    if [ $? -ne 0 ]; then
+      echo "Error: Unable to fetch client states from ${NODE}."
+      break
+    fi
     COUNTERPARTY_CHAIN=$(echo "${CLIENT_JSON}" | jq -r .client_state.chain_id)
     echo "${CHAIN_ID}, ${CLIENT}, ${CONNECTION}, ${CHANNEL}, ${PORT}, ${COUNTERPARTY_CHAIN}, ${COUNTERPARTY_CLIENT}, ${COUNTERPARTY_CONNECTION}, ${COUNTERPARTY_CHANNEL}, ${COUNTERPARTY_PORT}"
 done
