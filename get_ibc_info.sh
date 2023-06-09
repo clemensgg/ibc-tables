a4f10e673115ce29953ec1abb2b1fefff370bdd5
clemensgg <csc@soundportal.at>
2023-06-09 16:49:30 +0200

    only zero exit

--- get_ibc_info.sh
+++ get_ibc_info.sh
@@ -7,8 +7,7 @@ CHANNELS=""
 while true; do
   RESPONSE=$(curl -s --max-time 5 "${NODE}/ibc/core/channel/v1/channels?pagination.key=${PAGE_KEY}")
   if [[ -z "$RESPONSE" ]]; then
-    echo "Error: Unable to fetch channels from $NODE."
-    exit 1
+    exit 0
   fi
   CHANNELS_ARRAY=$(echo "${RESPONSE}" | jq -c '.channels[]')
   for CHANNEL in $CHANNELS_ARRAY; do
@@ -28,8 +27,7 @@ CONNECTIONS=""
 while true; do
   RESPONSE=$(curl -s --max-time 5 "${NODE}/ibc/core/connection/v1/connections?pagination.key=${PAGE_KEY}")
   if [[ -z "$RESPONSE" ]]; then
-    echo "Error: Unable to fetch connections from $NODE."
-    exit 1
+    exit 0
   fi
   CONNECTIONS_ARRAY=$(echo "${RESPONSE}" | jq -c '.connections[]')
   for CONNECTION in $CONNECTIONS_ARRAY; do
@@ -44,15 +42,13 @@ CONNECTIONS_JSON="[${CONNECTIONS%,}]"
 CONNECTIONS_JSON=$(echo "${CONNECTIONS_JSON}" | jq -r 'sort_by(.id | split("-")[1] | tonumber)')
 
 if [[ -z "$CHANNELS_JSON" || -z "$CONNECTIONS_JSON" ]]; then
-    echo "Error: Unable to fetch data from $NODE."
-    exit 1
+    exit 0
 fi
 
 NUM_CHANNELS=$(echo "${CHANNELS_JSON}" | jq '. | length')
 CHAIN_ID=$(curl -s -m 5 "${NODE}/cosmos/base/tendermint/v1beta1/blocks/latest" | jq -r .block.header.chain_id)
 if [ $? -ne 0 ]; then
-  echo "Error: Unable to fetch latest block from ${NODE}."
-  exit 1
+  exit 0
 fi
 echo "CHAIN_ID, CLIENT, CONNECTION, CHANNEL, PORT, COUNTERPARTY_CHAIN, COUNTERPARTY_CLIENT, COUNTERPARTY_CONNECTION, COUNTERPARTY_CHANNEL, COUNTERPARTY_PORT"
 for (( each_channel=0; each_channel < NUM_CHANNELS; each_channel++ ))
@@ -72,9 +68,9 @@ do
     CLIENT=$(echo "${CONNECTION_JSON}" | jq -r .client_id)
     CLIENT_JSON=$(curl -s -m 5 "${NODE}/ibc/core/client/v1/client_states/${CLIENT}")
     if [ $? -ne 0 ]; then
-      echo "Error: Unable to fetch client states from ${NODE}."
       break
     fi
     COUNTERPARTY_CHAIN=$(echo "${CLIENT_JSON}" | jq -r .client_state.chain_id)
     echo "${CHAIN_ID}, ${CLIENT}, ${CONNECTION}, ${CHANNEL}, ${PORT}, ${COUNTERPARTY_CHAIN}, ${COUNTERPARTY_CLIENT}, ${COUNTERPARTY_CONNECTION}, ${COUNTERPARTY_CHANNEL}, ${COUNTERPARTY_PORT}"
 done
+exit
